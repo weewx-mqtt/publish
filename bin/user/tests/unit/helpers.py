736fda3f9b8d1402be3db002ae79dd13cc4b971c
Rich Bell <bellrichm@gmail.com>
2025-12-30 09:01:01 -0500

    refactor

---
+++ bin/user/tests/unit/helpers.py
@@ -0,0 +1,26 @@
+#    Copyright (c) 2025 Rich Bell <bellrichm@gmail.com>
+#
+#    See the file LICENSE.txt for your full rights.
+#
+
+import importlib
+import sys
+import unittest
+
+def run_tests():
+    if len(sys.argv) == 1:
+        unittest.main(exit=False)
+        exit()
+    elif len(sys.argv) == 3:
+        filename = sys.argv[0].rsplit('/', 1)[-1]
+        module_name = filename.split('.', 11)[0]
+        module = importlib.import_module(module_name)
+
+        test_class = getattr(module, sys.argv[1])
+
+        test_suite = unittest.TestSuite()
+        test_suite.addTest(test_class(sys.argv[2]))
+        unittest.TextTestRunner().run(test_suite)
+        exit()
+
+    print("Bad parameters")
