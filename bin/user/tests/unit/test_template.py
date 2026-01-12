f14cc4f1ff701286475a06c04b00f037554a52aa
Rich Bell <bellrichm@gmail.com>
2026-01-12 16:16:05 -0500

    cleanup template

--- bin/user/tests/unit/test_template.py
+++ bin/user/tests/unit/test_template.py
@@ -1,17 +1,18 @@
-#    Copyright (c) 2025 Rich Bell <bellrichm@gmail.com>
+#    Copyright (c) 2025-2026 Rich Bell <bellrichm@gmail.com>
 #
 #    See the file LICENSE.txt for your full rights.
 #
 
+# pylint: disable=wrong-import-order
+# pylint: disable=missing-module-docstring, missing-class-docstring, missing-function-docstring
+# pylint: disable=invalid-name
 import unittest
+
+import helpers
 
 class TestTemplate(unittest.TestCase):
     def test_template(self):
         pass
 
 if __name__ == '__main__':
-    #test_suite = unittest.TestSuite()                  # noqa: E265
-    #test_suite.addTest(TestTemplate('test_template'))  # noqa: E265
-    #unittest.TextTestRunner().run(test_suite)          # noqa: E265
-
-    unittest.main(exit=False)                           # noqa: E265
+    helpers.run_tests()
