85ae3a261e45b01df2a3f01eb62ae3d092baf465
Rich Bell <bellrichm@gmail.com>
2026-01-19 15:18:51 -0500

    test configure_topics

--- bin/user/tests/unit/test_mqttpublish.py
+++ bin/user/tests/unit/test_mqttpublish.py
@@ -37,7 +37,49 @@ class TestInit(unittest.TestCase):
                     "'PublishWeeWX' is deprecated. Move options to top level, '[MQTTPublish]'.")
 
 class TestConfigureTopics(unittest.TestCase):
-    def test_test(self):
+    def test_config_topics(self):
+        mock_engine = mock.Mock()
+        config_dict = {
+            'MQTTPublish': {
+                'enable': False,
+            }
+        }
+        config = configobj.ConfigObj(config_dict)
+
+        topic1 = helpers.random_string()
+        service_dict = {
+            'topics': {
+                topic1: {},
+            },
+        }
+        service_config = configobj.ConfigObj(service_dict)
+
+        with mock.patch('user.mqttpublish.PublishWeeWXThread'):
+            with mock.patch('user.mqttpublish.Logger'):
+                SUT = user.mqttpublish.MQTTPublish(mock_engine, config)
+
+                topics_loop, topics_archive = SUT.configure_topics(service_config)
+
+                expected_topics = {
+                    topic1: {
+                        'qos': 0,
+                        'retain': False,
+                        'type': 'json',
+                        'unit_system': 1,
+                        'guarantee_delivery': False,
+                        'ignore': False,
+                        'append_unit_label': True,
+                        'conversion_type': 'string',
+                        'format': '%s',
+                        'fields': {},
+                        'aggregates': {}
+                    }
+                }
+
+                self.assertDictEqual(topics_loop, expected_topics)
+                self.assertDictEqual(topics_archive, expected_topics)
+
+    def test_config_topics_with_fields(self):
         mock_engine = mock.Mock()
         config_dict = {
             'MQTTPublish': {
@@ -48,16 +90,70 @@ class TestConfigureTopics(unittest.TestCase):
 
         topic1 = helpers.random_string()
         field1 = helpers.random_string()
-        aggregate1 = helpers.random_string()
         service_dict = {
             'topics': {
                 topic1: {
                     'fields': {
                         field1: {},
                     },
+                },
+            },
+        }
+        service_config = configobj.ConfigObj(service_dict)
+
+        with mock.patch('user.mqttpublish.PublishWeeWXThread'):
+            with mock.patch('user.mqttpublish.Logger'):
+                SUT = user.mqttpublish.MQTTPublish(mock_engine, config)
+
+                topics_loop, topics_archive = SUT.configure_topics(service_config)
+
+                expected_topics = {
+                    topic1: {
+                        'qos': 0,
+                        'retain': False,
+                        'type': 'json',
+                        'unit_system': 1,
+                        'guarantee_delivery': False,
+                        'ignore': False,
+                        'append_unit_label': True,
+                        'conversion_type': 'string',
+                        'format': '%s',
+                        'fields': {
+                            field1: {
+                                'name': field1,
+                                'unit': None,
+                                'ignore': False,
+                                'publish_none_value': False,
+                                'append_unit_label': True,
+                                'conversion_type': 'string',
+                                'format_string': '%s'
+                            }
+                        },
+                        'aggregates': {}
+                    }
+                }
+
+                self.assertDictEqual(topics_loop, expected_topics)
+                self.assertDictEqual(topics_archive, expected_topics)
+
+    def test_config_topics_with_aggregates(self):
+        mock_engine = mock.Mock()
+        config_dict = {
+            'MQTTPublish': {
+                'enable': False,
+            }
+        }
+        config = configobj.ConfigObj(config_dict)
+
+        topic1 = helpers.random_string()
+        aggreagate1 = helpers.random_string()
+        period = random.choice(list(user.mqttpublish.period_timespan.keys()))
+        service_dict = {
+            'topics': {
+                topic1: {
                     'aggregates': {
-                        aggregate1: {
-                            'period': random.choice(list(user.mqttpublish.period_timespan.keys()))
+                        aggreagate1: {
+                            'period': period,
                         },
                     },
                 },
@@ -71,9 +167,35 @@ class TestConfigureTopics(unittest.TestCase):
 
                 topics_loop, topics_archive = SUT.configure_topics(service_config)
 
-                print("done 1")
+                expected_topics = {
+                    topic1: {
+                        'qos': 0,
+                        'retain': False,
+                        'type': 'json',
+                        'unit_system': 1,
+                        'guarantee_delivery': False,
+                        'ignore': False,
+                        'append_unit_label': True,
+                        'conversion_type': 'string',
+                        'format': '%s',
+                        'fields': {},
+                        'aggregates': {
+                            aggreagate1: {
+                                'period': period,
+                                'name': aggreagate1,
+                                'unit': None,
+                                'ignore': False,
+                                'publish_none_value': False,
+                                'append_unit_label': True,
+                                'conversion_type': 'string',
+                                'format_string': '%s'
+                            }
+                        },
+                    }
+                }
 
-        print("done 2")
+                self.assertDictEqual(topics_loop, expected_topics)
+                self.assertDictEqual(topics_archive, expected_topics)
 
 if __name__ == '__main__':
     helpers.run_tests()
