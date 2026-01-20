0473fb7f952453473449f7cd7e55957a4ff114de
Rich Bell <bellrichm@gmail.com>
2026-01-20 11:13:03 -0500

    test update_field

--- bin/user/tests/unit/test_publishthread.py
+++ bin/user/tests/unit/test_publishthread.py
@@ -69,8 +69,7 @@ class TestPublishWeeWXThread(unittest.TestCase):
 
                     self.assertDictEqual(final_record, expected_record)
 
-    @unittest.skip("")
-    def test_template(self):
+    def test_update_field(self):
         mock_logger = mock.Mock()
         config_dict = {}
         config = configobj.ConfigObj(config_dict)
@@ -79,69 +78,44 @@ class TestPublishWeeWXThread(unittest.TestCase):
         topics_loop = None
         topics_archive = {}
         topics_archive = None
-        # data_queue = Queue.Queue()
         data_queue_mock = mock.Mock()
 
         SUT = user.mqttpublish.PublishWeeWXThread(mock_logger, config, topics_loop, topics_archive, data_queue_mock)
 
         field1 = helpers.random_string()
-        # aggreagate1 = helpers.random_string()
-        # period = random.choice(list(user.mqttpublish.period_timespan.keys()))
 
         topic_dict = {
-            # 'qos': 0,
-            # 'retain': False,
-            # 'type': 'json',
             'unit_system': 1,
-            # 'guarantee_delivery': False,
-            # 'ignore': False,
-            # 'append_unit_label': True,
-            # 'conversion_type': 'string',
-            'format': '%s',
             'fields': {
-            #    field1: {
-            #        'name': field1,
-            #        'unit': None,
-            #        'ignore': False,
-            #        'publish_none_value': False,
-            #        'append_unit_label': False,
-            #        'conversion_type': 'string',
-            #        'format_string': '%s'
-            #    }
-            },
-            'aggregates': {
-            #    aggreagate1: {
-            #        'period': period,
-            #        'name': aggreagate1,
-            #        'unit': None,
-            #        'ignore': False,
-            #        'publish_none_value': False,
-            #        'append_unit_label': False,
-            #        'conversion_type': 'string',
-            #        'format_string': '%s',
-            #        'observation': helpers.random_string(),
-            #        'aggregation': helpers.random_string(),
-            #    },
+                field1: {
+                    'unit': 'foobar',
+                    'append_unit_label': True,
+                    'conversion_type': 'float',
+                    'format': '%s'
+                }
             },
         }
 
-        record = {
-            'dateTime': time.time(),
-            'usUnits': 1,
-        }
         updated_record = {
             field1: random.random(),
             'usUnits': 1,
         }
+        converted_value = random.random()
+        unit_type = helpers.random_string()
 
-        with mock.patch.object(user.mqttpublish.weewx.units, 'to_std_system', return_value=updated_record):
-            with mock.patch.object(user.mqttpublish.weewx.xtypes, 'get_aggregate'):
-                with mock.patch.object(user.mqttpublish.weewx.units, 'convertStd', return_value=[random.random()]):
+        with mock.patch.object(user.mqttpublish.weewx.units,
+                               'getStandardUnitType',
+                               return_value=(unit_type, helpers.random_string())):
+            with mock.patch.object(user.mqttpublish.weewx.units, 'convert', return_value=[converted_value]):
 
-                    final_record = SUT.update_record(topic_dict, record)
-                    print(final_record)
+                (name, value) = SUT.update_field(topic_dict,
+                                                 topic_dict['fields'][field1],
+                                                 field1,
+                                                 updated_record[field1],
+                                                 updated_record['usUnits'])
 
-        print("done")
+                self.assertEqual(name, f"{field1}_{unit_type}")
+                self.assertEqual(value, converted_value)
 
 if __name__ == '__main__':
     helpers.run_tests()
