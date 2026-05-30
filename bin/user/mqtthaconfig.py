b3ac756dc26aee2a40bba6f9f0a26fb5395093c1
Rich Bell <bellrichm@gmail.com>
2026-05-30 13:46:19 -0400

    checkpoint device class

--- bin/user/mqtthaconfig.py
+++ bin/user/mqtthaconfig.py
@@ -46,6 +46,212 @@ CONFIG_STR = """
             topic = status
             payload_available = online
             payload_not_available = offline
+    [device_data]
+        [[dateTime]]
+            # class = timestamp
+        [[usUnits]]
+            class = enum
+        [[interval]]
+        [[altimeter]]
+            class = atmospheric_pressure
+        [[appTemp]]
+            class = temperature
+        [[appTemp1]]
+            class = temperature
+        [[barometer]]
+            class = atmospheric_pressure
+        [[batteryStatus1]]
+            class = battery
+        [[batteryStatus2]]
+            class = battery
+        [[batteryStatus3]]
+            class = battery
+        [[batteryStatus4]]
+            class = battery
+        [[batteryStatus5]]
+            class = battery
+        [[batteryStatus6]]
+            class = battery
+        [[batteryStatus7]]
+            class = battery
+        [[batteryStatus8]]
+            class = battery
+        [[cloudbase]]
+            class = distance
+        [[co]]
+            class = carbon_monoxide
+        [[co2]]
+            class = carbon_dioxide
+        [[consBatteryVoltage]]
+            class = voltage
+        [[dewpoint]]
+            class = temperature
+        [[dewpoint1]]
+            class = temperature
+        [[ET]]
+        [[extraHumid1]]
+            class = humidity
+        [[extraHumid2]]
+            class = humidity
+        [[extraHumid3]]
+            class = humidity
+        [[extraHumid4]]
+            class = humidity
+        [[extraHumid5]]
+            class = humidity
+        [[extraHumid6]]
+            class = humidity
+        [[extraHumid7]]
+            class = humidity
+        [[extraHumid8]]
+            class = humidity
+        [[extraTemp1]]
+            class = temperature
+        [[extraTemp2]]
+            class = temperature
+        [[extraTemp3]]
+            class = temperature
+        [[extraTemp4]]
+            class = temperature
+        [[extraTemp5]]
+            class = temperature
+        [[extraTemp6]]
+            class = temperature
+        [[extraTemp7]]
+            class = temperature
+        [[extraTemp8]]
+            class = temperature
+        [[forecast]]
+        [[hail]]
+            class = precipitation
+        [[hailBatteryStatus]]
+            class = battery
+        [[hailRate]]
+        [[heatindex]]
+            class = temperature
+        [[heatindex1]]
+            class = temperature
+        [[heatingTemp]]
+            class = temperature
+        [[heatingVoltage]]
+            class = voltage
+        [[humidex]]
+        [[humidex1]]
+        [[illuminance]]
+            class = illuminance
+        [[inDewpoint]]
+            class = temperature
+        [[inHumidity]]
+            class = humidity
+        [[inTemp]]
+            class = temperature
+        [[inTempBatteryStatus]]
+            class = battery
+        [[leafTemp1]]
+            class = temperature
+        [[leafTemp2]]
+            class = temperature
+        [[leafWet1]]
+            class = moisture
+        [[leafWet2]]
+            class = moisture
+        [[lightning_distance]]
+            class = distance
+        [[lightning_disturber_count]]
+        [[lightning_energy]]
+        [[lightning_noise_count]]
+        [[lightning_strike_count]]
+        [[luminosity]]
+        [[maxSolarRad]]
+            class = irradiance
+        [[nh3]]
+        [[no2]]
+            class = nitogen_dioxide
+        [[noise]]
+            class = sound_pressure
+        [[o3]]
+            class = ozone
+        [[outHumidity]]
+            class = humidity
+        [[outTemp]]
+            class = temperature
+        [[outTempBatteryStatus]]
+            class = battery
+        [[pb]]
+        [[pm10_0]]
+            class = pm10
+        [[pm1_0]]
+            class = pm1
+        [[pm2_5]]
+            class = pm25
+        [[pressure]]
+            class = atmospheric_pressure
+        [[radiation]]
+            class = irradiance
+        [[rain]]
+            class = radiation
+        [[rainBatteryStatus]]
+            class = battery
+        [[rainRate]]
+            class = precipitation_intensity
+        [[referenceVoltage]]
+            class = voltage
+        [[rxCheckPercent]]
+        [[signal1]]
+        [[signal2]]
+        [[signal3]]
+        [[signal4]]
+        [[signal5]]
+        [[signal6]]
+        [[signal7]]
+        [[signal8]]
+        [[snow]]
+            class = precipitation
+        [[snowBatteryStatus]]
+            class = battery
+        [[snowDepth]]
+            class = distance
+        [[snowMoisture]]
+            class = moisture
+        [[snowRate]]
+            class = precipitation_intensity
+        [[so2]]
+            class = sulphur_dioxide
+        [[soilMoist1]]
+            class = moisture
+        [[soilMoist2]]
+            class = moisture
+        [[soilMoist3]]
+            class = moisture
+        [[soilMoist4]]
+            class = moisture
+        [[soilTemp1]]
+            class = temperature
+        [[soilTemp2]]
+            class = temperature
+        [[soilTemp3]]
+            class = temperature
+        [[soilTemp4]]
+            class = temperature
+        [[supplyVoltage]]
+            class = voltage
+        [[txBatteryStatus]]
+            class = battery
+        [[UV]]
+        [[uvBatteryStatus]]
+            class = battery
+        [[windBatteryStatus]]
+            class = battery
+        [[windchill]]
+            class = temperature
+        [[windDir]]
+        [[windGust]]
+            class = wind_speed
+        [[windGustDir]]
+        [[windrun]]
+        [[windSpeed]]
+            class = wind_speed
+
 """
 
 class MQTTHomeAssistantConfig:
@@ -139,8 +345,11 @@ class MQTTHomeAssistantConfig:
                             'platform': 'sensor',
                             'value_template': value_template,
                             'unique_id': field,
-                            'name': field,
+                            'name': self.defaults_dict['Labels']['Generic'].get(field, field),
                         }
+                        #if self.config['device_data'].get(field, {}).get('class'):
+                        #    self.config['devices'][device_id]['components'][field]['device_class'] = \
+                        #        self.config['device_data'][field].get('class')
 
                 if new_sensor:
                     payload = json.dumps(self.config['devices'][device_id])
