8bb4ee65a1ad6fef53397df0505d28fa9c091d69
Rich Bell <bellrichm@gmail.com>
2026-06-12 17:51:08 -0400

    tests

--- bin/user/tests/unit/test_mqtthaconfig.py
+++ bin/user/tests/unit/test_mqtthaconfig.py
@@ -7,14 +7,55 @@
 # pylint: disable=missing-module-docstring, missing-class-docstring, missing-function-docstring
 # pylint: disable=invalid-name
 import unittest
+import mock
+
+import configobj
 
 import helpers
 
 import user.mqtthaconfig
 
-class TestTemplate(unittest.TestCase):
-    def test_template(self):
-        pass
+class test_MQTTHomeAssistantConfig(unittest.TestCase):
+    def test_get_callbacks(self):
+        mock_logger = mock.Mock()
+        name = helpers.random_string()
+        plugin_dict = {
+            'devices': {},
+            'component_data': {}
+        }
+        weewx_dict = {
+            'defaults': {}
+        }
+
+        SUT = user.mqtthaconfig.MQTTHomeAssistantConfig(mock_logger, name, configobj.ConfigObj(plugin_dict), weewx_dict)
+
+        callbacks = SUT.get_callbacks()
+
+        expected_callbacks =  [
+            {
+                'on_weewx_data': {
+                    'timing': 'immediate',
+                    'callback': SUT.on_weewx_data
+                }
+            },
+            {
+                'on_mqtt_connect': {
+                    'timing': 'immediate',
+                    'callback': SUT.on_mqtt_connect
+                }
+            },
+            {
+                'on_mqtt_message': {
+                    'timing': 'immediate',
+                    'callback': SUT.on_mqtt_message
+                },
+                'update_record': {
+                    'timing': 'delay',
+                    'callback': SUT.update_record
+                },
+            },
+        ]
+        self.assertEqual(callbacks, expected_callbacks)
 
 if __name__ == '__main__':
     helpers.run_tests()
